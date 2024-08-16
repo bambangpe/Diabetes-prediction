6a02c3562d1d5d13fed77f2e93adca8113ed3df4
bambangpe pe <pebambang@gmail.com>
2024-08-16 15:19:51 +0700

    Update DIAB.py

--- DIAB.py
+++ DIAB.py
@@ -4,9 +4,9 @@ import streamlit as st
 from sklearn.preprocessing import StandardScaler
 from streamlit_option_menu import option_menu
 
-Diabetes_trained_model = pickle.load(open('models/diabetes_trained_model.sav', 'rb'))
-heart_trained_model = pickle.load(open('models/heart_trained_model.sav', 'rb'))
-parkinson_trained_model = pickle.load(open('models/parkinson_trained_model.sav', 'rb'))
+Diabetes_trained_model = pickle.load(open('diabetes_trained_model.sav', 'rb'))
+#heart_trained_model = pickle.load(open('models/heart_trained_model.sav', 'rb'))
+#parkinson_trained_model = pickle.load(open('models/parkinson_trained_model.sav', 'rb'))
 
 # Sidebar for Navigation
 with st.sidebar:
