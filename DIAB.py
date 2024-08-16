177193577194fb5dafa18ce23e0faff64128cbb4
bambangpe pe <pebambang@gmail.com>
2024-08-16 15:26:36 +0700

    Update DIAB.py

--- DIAB.py
+++ DIAB.py
@@ -1,66 +1,55 @@
 import numpy as np
 import pickle
 import streamlit as st
-from sklearn.preprocessing import StandardScaler
-from streamlit_option_menu import option_menu
-
-Diabetes_trained_model = pickle.load(open('diabetes_trained_model.sav', 'rb'))
-#heart_trained_model = pickle.load(open('models/heart_trained_model.sav', 'rb'))
-#parkinson_trained_model = pickle.load(open('models/parkinson_trained_model.sav', 'rb'))
-
-# Sidebar for Navigation
-with st.sidebar:
- selected = option_menu('Multiple Disease Prediction System',
- ['Diabetes Prediction',
- 'Heart Disease Prediction',
- 'Parkinsons Prediction'],
- icons=['activity','heart','person'],
- default_index=0)
- 
-# Creating a function for prediction
-def diabetes_prediction(input_data):
- # changing the input_data to numpy array
- input_data_as_numpy_array = np.asarray(input_data)
 
 
-# reshape the array as we are predicting for one instance
- input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+# loading the saved model
+#loaded_model = pickle.load(open('Diabetes-prediction/trained_model.sav', 'rb'))
+with open('Diabetes-prediction/trained_model.sav', 'rb') as loaded_model:
+    # creating a function for Prediction
+    def diabetes_prediction(input_data):
+        # changing the input_data to numpy array
+        input_data_as_numpy_array = np.asarray(input_data)
 
-scaler = StandardScaler()
- # standardize the input data
- std_data = scaler.fit_transform(input_data_reshaped)
+        # reshape the array as we are predicting for one instance
+        input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 
-prediction = Diabetes_trained_model.predict(std_data)
- # print(prediction)
+        prediction = loaded_model.predict(input_data_reshaped)
+        print(prediction)
 
-if (prediction[0] == 0):
- return 'The person is not diabetic'
- else:
- return 'The person is diabetic'
- 
-def main():
- # Giving Title
- st.title('Diabetes Predction Web App')
- 
- # Getting input data from the user
- # Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age
- Pregnancies = st.text_input('Number of Pregnancies')
- Glucose = st.text_input('Glucose Level')
- BloodPressure = st.text_input('Blood Pressure Level')
- SkinThickness = st.text_input('Skin Thickness Value')
- Insulin = st.text_input('Insulin Level')
- BMI = st.text_input('BMI Value')
- DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function')
- Age = st.text_input('Age of the Person')
- 
- # Code for prediction
- diagnosis = ''
- 
- # Creating button for Prediction
- if st.button('Diabetes Test Result'):
- diagnosis = diabetes_prediction([Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age])
- 
- st.success(diagnosis)
- 
-if __name__ == '__main__':
- main()
+        if (prediction[0] == 0):
+            return 'The person is not diabetic'
+        else:
+            return 'The person is diabetic'
+  
+    def main():
+        # giving a title
+        st.title('Diabetes Prediction in women Web App')
+    
+    
+        # getting the input data from the user
+    
+    
+        Pregnancies = st.text_input('Number of Pregnancies')
+        Glucose = st.text_input('Glucose Level')
+        BloodPressure = st.text_input('Blood Pressure value')
+        SkinThickness = st.text_input('Skin Thickness value')
+        Insulin = st.text_input('Insulin Level')
+        BMI = st.text_input('BMI value')
+        DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
+        Age = st.text_input('Age of the Person')
+    
+    
+        # code for Prediction
+        diagnosis = ''
+    
+        # creating a button for Prediction
+    
+        if st.button('Diabetes Test Result'):
+            diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
+        
+        
+        st.success(diagnosis)
+    
+    if __name__ == '__main__':
+        main()
